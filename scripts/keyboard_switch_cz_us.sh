02fd61ef96363e8736937124f264e2d828a9e955
gamseb <gamseb@gmail.com>
2019-07-05 20:21:55 +0200

    Initial commit
    Creating the initial commit for my dotfiles

---
+++ scripts/keyboard_switch_cz_us.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+CURRENT_KEYBOARD="$(setxkbmap -query | grep layout | tail -c 3)"
+echo "$CURRENT_KEYBOARD"
+if [ $CURRENT_KEYBOARD == "cz" ]; then
+  notify-send "Setting the keyboard to english"
+  setxkbmap us
+elif [ $CURRENT_KEYBOARD == "us" ]; then 
+  notify-send "Setting the keyboard to czech"
+  setxkbmap cz
+fi
